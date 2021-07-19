615e3835711c34b8d92819852357d26183e6cec0
Tiago Sanchez <tiagofsanchez@gmail.com>
2021-07-19 17:28:32 +0800

    graphql defenitions

--- keystone.ts
+++ keystone.ts
@@ -1,30 +1,31 @@
-import { config } from '@keystone-next/keystone/schema';
-import { statelessSessions } from '@keystone-next/keystone/session';
-import { createAuth } from '@keystone-next/auth';
+import { config } from "@keystone-next/keystone/schema";
+import { statelessSessions } from "@keystone-next/keystone/session";
+import { createAuth } from "@keystone-next/auth";
 
-import { lists } from './schema';
+import { lists } from "./schema";
 
-let sessionSecret = process.env.SESSION_SECRET || '8d8fb9be-25db-4220-bd8d-9acbc56d73f8';
+let sessionSecret =
+  process.env.SESSION_SECRET || "8d8fb9be-25db-4220-bd8d-9acbc56d73f8";
 
 if (!sessionSecret) {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "production") {
     throw new Error(
-      'The SESSION_SECRET environment variable must be set in production'
+      "The SESSION_SECRET environment variable must be set in production"
     );
   } else {
-    sessionSecret = '-- DEV COOKIE SECRET; CHANGE ME --';
+    sessionSecret = "-- DEV COOKIE SECRET; CHANGE ME --";
   }
 }
 
 let sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
 
 const { withAuth } = createAuth({
-  listKey: 'User',
-  identityField: 'email',
-  secretField: 'password',
-  sessionData: 'name',
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  sessionData: "name",
   initFirstItem: {
-    fields: ['name', 'email', 'password'],
+    fields: ["name", "email", "password"],
   },
 });
 
@@ -36,13 +37,20 @@ const session = statelessSessions({
 export default withAuth(
   config({
     db: {
-      adapter: 'prisma_postgresql',
-      url: process.env.DATABASE_URL || 'postgres://tfs@localhost/keystone-render',
+      adapter: "prisma_postgresql",
+      url:
+        process.env.DATABASE_URL || "postgres://tfs@localhost/keystone-render",
     },
     ui: {
       isAccessAllowed: (context) => !!context.session?.data,
     },
     lists,
     session,
+    graphql: {
+      apolloConfig: {
+        playground: true,
+        introspection: true,
+      },
+    },
   })
 );
